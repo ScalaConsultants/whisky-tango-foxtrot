b055a2a6e2bd0dea6289c6df6bf782b6b59e0011
Marcin Gorczyński <margorczynski@gmail.com>
2016-05-17 15:34:51 +0200

    Added basic user data validation to showcase Validate

--- src/main/scala/io/scalac/wtf/domain/User.scala
+++ src/main/scala/io/scalac/wtf/domain/User.scala
@@ -1,7 +1,9 @@
 package io.scalac.wtf.domain
 
+import cats.data.Validated.{ invalidNel, valid }
+import cats.data.{ NonEmptyList, ValidatedNel }
+import cats.implicits._
 import slick.lifted.MappedTo
-
 
 case class UserId(value: Long) extends MappedTo[Long]
 
@@ -9,3 +11,26 @@ case class User(
     id: Option[UserId] = None,
     email: String, 
     password: String)
+
+object User {
+
+  sealed trait ValidationError
+  final case object EmptyEmail    extends ValidationError
+  final case object EmptyPassword extends ValidationError
+
+  def validateUser(email: String, password: String): ValidatedNel[ValidationError, User] = {
+    val validation = (validateEmail(email) |@| validatePassword(password))
+
+    (validation) map { case (email, password) => User(email = email, password = password)}
+  }
+
+  private def validateEmail(email: String): ValidatedNel[ValidationError, String] = email match {
+    case "" => invalidNel(EmptyEmail)
+    case _  => valid(email)
+  }
+
+  private def validatePassword(password: String): ValidatedNel[ValidationError, String] = password match {
+    case "" => invalidNel(EmptyPassword)
+    case _  => valid(password)
+  }
+}
