c2fcade036d9467d21adca2b3a0b5afd06f68942
Marcin Gorczyński <margorczynski@gmail.com>
2016-05-24 15:37:02 +0200

    Aligned the tests to the new API and logic

--- src/test/scala/io/scalac/wtf/domain/UserServiceSpec.scala
+++ src/test/scala/io/scalac/wtf/domain/UserServiceSpec.scala
@@ -10,8 +10,13 @@ import slick.driver.H2Driver._
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-
 import UserService.createUser
+import cats.data.Validated.{Invalid, Valid}
+import io.scalac.wtf.domain.User.{PasswordTooShort, WrongEmailPattern}
+
+import cats.implicits._
+
+import scala.util.Success
 
 class UserServiceSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
@@ -36,62 +41,114 @@ class UserServiceSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   "An UserService" should "create an user when it's valid and e-mail not taken" in {
     val createUserWork = for {
-      _ <- createUser(validUser)
-    } yield ()
+      result <- createUser(validUser)
+    } yield result
 
     val resultFuture = db.run(createUserWork)
 
     resultFuture.onComplete { result =>
       assert(result.isSuccess)
+
+      result match {
+        case Success(r) => assert(r.isValid)
+        case _          => assert(false)
+      }
     }
   }
 
   "An UserService" should "not create an user when it's e-mail is not valid" in {
     val createUserWork = for {
-      _ <- createUser(noEmailUser)
-    } yield ()
+      result <- createUser(noEmailUser)
+    } yield result
 
     val resultFuture = db.run(createUserWork)
 
     resultFuture.onComplete { result =>
-      assert(result.isFailure)
+      assert(result.isSuccess)
+
+      result match {
+        case Success(r) => assert(r.isInvalid)
+        case _          => assert(false)
+      }
     }
   }
 
   "An UserService" should "not create an user when it's password is not valid" in {
     val createUserWork = for {
-      _ <- createUser(noPasswordUser)
-    } yield ()
+      result <- createUser(noPasswordUser)
+    } yield result
 
     val resultFuture = db.run(createUserWork)
 
     resultFuture.onComplete { result =>
-      assert(result.isFailure)
+      assert(result.isSuccess)
+
+      result match {
+        case Success(r) => assert(r.isInvalid)
+        case _          => assert(false)
+      }
     }
   }
 
   "An UserService" should "not create an user when the credentials are empty" in {
     val createUserWork = for {
-      _ <- createUser(noCredentailsUser)
-    } yield ()
+      result <- createUser(noCredentailsUser)
+    } yield result
 
     val resultFuture = db.run(createUserWork)
 
     resultFuture.onComplete { result =>
-      assert(result.isFailure)
+      assert(result.isSuccess)
+
+      result match {
+        case Success(r) => assert(r.isInvalid)
+        case _          => assert(false)
+      }
     }
   }
 
   "An UserService" should "not create an user when it's the e-mail is taken" in {
     val createUserWork = for {
-      _ <- createUser(validUser)
-      _ <- createUser(validUser)
-    } yield ()
+      _      <- createUser(validUser)
+      result <- createUser(validUser)
+    } yield result
 
     val resultFuture = db.run(createUserWork)
 
     resultFuture.onComplete { result =>
-      assert(result.isFailure)
+      assert(result.isSuccess)
+
+      result match {
+        case Success(r) => assert(r.isInvalid)
+        case _          => assert(false)
+      }
+    }
+  }
+
+  "An UserService" should "not create an user when the credentials are empty and return 2 validation errors in the result" in {
+    val createUserWork = for {
+      result <- createUser(noCredentailsUser)
+    } yield result
+
+    val resultFuture = db.run(createUserWork)
+
+    resultFuture.onComplete { result =>
+      assert(result.isSuccess)
+
+      result match {
+        case Success(r) => {
+          assert(r.isInvalid)
+          r match {
+            case Valid(_) => assert(false)
+            case Invalid(e) => {
+              val errorList = e.unwrap
+              assert(errorList.contains(WrongEmailPattern))
+              assert(errorList.contains(PasswordTooShort))
+            }
+          }
+        }
+        case _ => assert(false)
+      }
     }
   }
 
