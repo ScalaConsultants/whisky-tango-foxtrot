f2181182e6f4b80db7476435f624e087ac1dcef6
Marcin Gorczyński <margorczynski@gmail.com>
2016-06-20 15:52:50 +0200

    Moved dependancies into Config. Use actorSystem execution context

--- src/main/scala/io/scalac/wtf/domain/WtfApplication.scala
+++ src/main/scala/io/scalac/wtf/domain/WtfApplication.scala
@@ -1,12 +1,6 @@
 package io.scalac.wtf.domain
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-
 trait WtfApplication extends App
+  with AkkaDependancy
   with DatabaseDependancy
-  with UserRoutes {
-
-  implicit val actorSystem       = ActorSystem("wtf-system")
-  implicit val actorMaterializer = ActorMaterializer()
-}
+  with UserRoutes
